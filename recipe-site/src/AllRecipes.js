a919d6c8bfe2cc4407c75277e96a53bec206ea7a
Alya Campbell <94196424+alyarshelle@users.noreply.github.com>
2024-06-25 20:40:54 +0000

    router

---
+++ recipe-site/src/AllRecipes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function AllRecipes() {
+    return ( 
+        <div>   
+        <section>
+            <h1>Simple Recipes For Your Busy Lives</h1> 
+        </section>
+
+        <section>
+            <h1>Recipes For Your Busy Lives</h1> 
+        </section>
+
+        </div>
+     );
+    }
+    
+export default AllRecipes;
--- recipe-site/src/App.js
+++ recipe-site/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css'; 
 import Navbar from './Navbar'; // Import the Navbar component
 import LandingPage from './LandingPage';
+import AllRecipes from './AllRecipes';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingPage /> 
+      <AllRecipes />
     </div>
   );
 }
