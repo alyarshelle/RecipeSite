c5f42e50920b6c90ec4df622157c0028fb83cd4d
Alya Campbell <94196424+alyarshelle@users.noreply.github.com>
2024-07-08 18:58:30 +0000

    styling

--- recipe-site/src/RecipePages/AllRecipes.js
+++ recipe-site/src/RecipePages/AllRecipes.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import food from '../Images/food.jpg';
+import './AllRecipes.css';
 
 function AllRecipes() {
-    return ( 
-        <div>   
-        <section>
-            <h1>HEYO</h1> 
-        </section>
+    return (
+        <div>
+            <section className="food-section">
+                <div className="image-container">
+                    <img id="image" src={food} alt="food_background"/>
+                    <div className="image-text">
+                        <h2 id='title1'>Crafty Recipes</h2>
+                        <p id="title2">Updated Daily</p>
+                    </div>
+                </div>
+            </section>
 
-        <section>
-            <h1>Busy Lives</h1> 
-        </section>
-
+            <section>
+                <h2>HYEO</h2>
+            </section>
         </div>
-     );
-    }
-    
+    );
+}
+
 export default AllRecipes;
